7e6dd4b75e21eca52010112d553e1987f6f1d01b
Mufadal03 <sadrimufadal@gmail.com>
2023-03-03 21:51:36 +0530

    v1

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const {connection} =  require('./config/db')
+const { mediaController } = require('./routes/media.routes')
 require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
 const PORT = process.env.PORT || 8080
+
+app.use('/media',mediaController)
+
 app.listen(PORT, async () => {
     try {
         await connection
