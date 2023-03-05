a8efd4749d22a6b776c420a000aecb8a677cc97a
Mufadal03 <sadrimufadal@gmail.com>
2023-03-05 14:44:17 +0530

    all routes

---
+++ client/src/routes/AllRoutes.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import {Route, Routes} from 'react-router-dom'
+import Home from '../pages/Home'
+import MediaDetail from '../pages/MediaDetail'
+import PersonDetail from '../pages/PersonDetail'
+import MediaByGenre from '../pages/MediaByGenre'
+const AllRoutes = () => {
+  return (
+      <>
+          <Navbar />
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/:mediaType' element={<MediaByGenre/>} /> 
+              <Route path='/detail/:mediaType/:mediaId' element={<MediaDetail />} /> 
+              <Route path='/person/:personId' element={<PersonDetail />} />
+              <Route path='*' element={<h1>404 notfound</h1>} />
+            </Routes>
+      </>
+  )
+}
+
+export default AllRoutes
