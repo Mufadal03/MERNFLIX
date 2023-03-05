38077565c4c6577e2fe46b194f503a5310f82a5a
Mufadal03 <sadrimufadal@gmail.com>
2023-03-05 11:32:46 +0530

    new routes to index

--- server/index.js
+++ server/index.js
@@ -2,14 +2,18 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const {connection} =  require('./config/db')
-const { mediaController } = require('./routes/media.routes')
+const { mediaRoutes } = require('./routes/media.routes')
+const { personRoutes } = require('./routes/person.routes')
+const { userRoutes } = require('./routes/user.routes')
 require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
 const PORT = process.env.PORT || 8080
 
-app.use('/media',mediaController)
+app.use('/media', mediaRoutes)
+app.use('/person', personRoutes)
+app.use('/user',userRoutes)
 
 app.listen(PORT, async () => {
     try {
