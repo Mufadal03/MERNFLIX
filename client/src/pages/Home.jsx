246b2633332d54f89e107243ceb261fd5876e495
Mufadal03 <sadrimufadal@gmail.com>
2023-03-05 14:44:47 +0530

    pages

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <Box h='200vh' >Home</Box>
+  )
+}
+
+export default Home
---
+++ client/src/pages/MediaByGenre.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MediaByGenre = () => {
+  return (
+    <div>MediaByGenre</div>
+  )
+}
+
+export default MediaByGenre
