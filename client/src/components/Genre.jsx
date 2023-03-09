52aaafab68ad00952ebd6df6846923675b7849a5
Mufadal03 <sadrimufadal@gmail.com>
2023-03-09 22:59:52 +0530

    Genres

--- client/src/components/Genre.jsx
+++ client/src/components/Genre.jsx
@@ -6,14 +6,14 @@ import GenreRow from './GenreRow'
 const Genre = () => {
   return (
       <Flex direction={'column'} gap='1rem' pl='2rem' bgColor={'black'} color='white'>
-          <GenreRow title={'trending movies'} api={mediaApi.getTrendingList({ mediaType: 'movie', timeWindow: 'week', page: 1 })} />
-          <GenreRow title={'trending Tv shows'} api={mediaApi.getTrendingList({mediaType:'tv',timeWindow:'week'}) } />
-          <GenreRow title={'popular movies' } api={mediaApi.getList({mediaType:'movie',mediaCategory:'popular',page:1})} />
-          <GenreRow title={'popular tv shows'} api={ mediaApi.getList({mediaType:'tv',mediaCategory:'popular',page:1})} />
-          <GenreRow title={'Science Fiction movies'} api={mediaApi.getMediaByGenre({ mediaType: 'movie', genreId: '878' })} />
-          <GenreRow title={'thriller movies'} api={mediaApi.getMediaByGenre({ mediaType: 'movie', genreId: '53' })} /> 
-          <GenreRow title={'Animation movies' } api={mediaApi.getMediaByGenre({mediaType:'movie',genreId:'16'})} /> 
-          <GenreRow title={'Documentary' } api={mediaApi.getMediaByGenre({mediaType:'tv',genreId:'99'})} />
+          <GenreRow title={'trending movies'} api={mediaApi.getTrendingList({ mediaType: 'movie', timeWindow: 'week', page: 1 })} to='/media/movie?category=trending'/>
+          <GenreRow title={'trending Tv shows'} api={mediaApi.getTrendingList({mediaType:'tv',timeWindow:'week'}) } to='/media/tv?category=trending' />
+          <GenreRow title={'popular movies' } api={mediaApi.getList({mediaType:'movie',mediaCategory:'popular',page:1})} to='/media/movie?category=popular' />
+          <GenreRow title={'popular tv shows'} api={ mediaApi.getList({mediaType:'tv',mediaCategory:'popular',page:1})} to='/media/tv?category=popular'/>
+          <GenreRow title={'Science Fiction movies'} api={mediaApi.getMediaByGenre({ mediaType: 'movie', genreId: '878' })} to='/media/movie?category=sci-fic' />
+          <GenreRow title={'thriller movies'} api={mediaApi.getMediaByGenre({ mediaType: 'movie', genreId: '53' })} to='/media/movie?category=thriller'/> 
+          <GenreRow title={'Animation movies' } api={mediaApi.getMediaByGenre({mediaType:'movie',genreId:'16'})} to='/media/movie?category=animation'/> 
+          <GenreRow title={'Documentary' } api={mediaApi.getMediaByGenre({mediaType:'tv',genreId:'99'})} to='/media/tv?category=documentory'/>
           
     </Flex>
   )
