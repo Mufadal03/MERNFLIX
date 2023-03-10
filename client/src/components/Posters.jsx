ae2577ec5c7bd595c642128466498a1ab37f7e29
Mufadal03 <sadrimufadal@gmail.com>
2023-03-10 22:23:08 +0530

    Media poster slider

---
+++ client/src/components/Posters.jsx
@@ -0,0 +1,37 @@
+import { Box, Flex, Heading, Image } from '@chakra-ui/react'
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Scrollbar, A11y } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
+import tmdbConfig from '../api/tmdb/tmdb.config';
+import './styles/slides.css'
+const Posters = ({ data }) => {
+  return (
+    (
+              <Box  w='90vw' m='auto'>
+              <Heading fontWeight={'md'} py='1rem'>POSTERS</Heading>
+          <Swiper
+              modules={[Navigation, Scrollbar, A11y]}
+              spaceBetween={10}
+              slidesPerView={5}
+              grabCursor='true'
+              navigation
+          >
+              {
+                  data?.length > 0 && data?.map((el, i) => {
+                      return (
+                          <SwiperSlide key={i}>
+                              <Image src={tmdbConfig.posterImgUrl(el.file_path)} />
+                          </SwiperSlide>
+                      )
+                  })
+              }
+                  </Swiper>
+        </Box>
+  )
+  )
+}
+
+export default Posters
