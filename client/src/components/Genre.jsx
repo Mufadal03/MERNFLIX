1cd3bb0c9924147d04f39787e4204fcb476529ae
Mufadal03 <sadrimufadal@gmail.com>
2023-03-10 22:19:36 +0530

    changed trending url to day

--- client/src/components/Genre.jsx
+++ client/src/components/Genre.jsx
@@ -6,8 +6,8 @@ import GenreRow from './GenreRow'
 const Genre = () => {
   return (
       <Flex direction={'column'} gap='1rem' pl='2rem' bgColor={'black'} color='white'>
-          <GenreRow title={'trending movies'} api={mediaApi.getTrendingList({ mediaType: 'movie', timeWindow: 'week', page: 1 })} to='/media/movie?category=trending'/>
-          <GenreRow title={'trending Tv shows'} api={mediaApi.getTrendingList({mediaType:'tv',timeWindow:'week'}) } to='/media/tv?category=trending' />
+          <GenreRow title={'trending movies'} api={mediaApi.getTrendingList({ mediaType: 'movie', timeWindow: 'day', page: 1 })} to='/media/movie?category=trending'/>
+          <GenreRow title={'trending Tv shows'} api={mediaApi.getTrendingList({mediaType:'tv',timeWindow:'day'}) } to='/media/tv?category=trending' />
           <GenreRow title={'popular movies' } api={mediaApi.getList({mediaType:'movie',mediaCategory:'popular',page:1})} to='/media/movie?category=popular' />
           <GenreRow title={'popular tv shows'} api={ mediaApi.getList({mediaType:'tv',mediaCategory:'popular',page:1})} to='/media/tv?category=popular'/>
           <GenreRow title={'Science Fiction movies'} api={mediaApi.getMediaByGenre({ mediaType: 'movie', genreId: '878' })} to='/media/movie?category=sci-fic' />
