e92dde04b7f8749523988c6e12903e8c3482ad07
Mufadal03 <sadrimufadal@gmail.com>
2023-03-10 22:18:56 +0530

    aspect/ratio fixed

--- client/src/components/CompanyCard.jsx
+++ client/src/components/CompanyCard.jsx
@@ -5,8 +5,8 @@ import tmdbConfig from '../api/tmdb/tmdb.config'
 const CompanyCard = ({ imgUrl, name }) => {
     
   return (
-      <Flex direction={'column'} gap='.8rem' border={'1px solid rgba(115, 114, 114, 0.5)'} borderRadius='lg' p='2' alignItems='center' >
-          {imgUrl && <Box bgImage={`url(${tmdbConfig.companyImgUrl(imgUrl)})`} h='100px' w='150px' bgPos={'center'} bgSize='cover' objectFit={'cover'}></Box>}
+      <Flex direction={'column'} gap='.8rem' border={'1px solid rgba(115, 114, 114, 0.5)'} borderRadius='lg' p='2' alignItems='center' bgColor={'rgba(80, 79, 79, 0.5)'}>
+          {imgUrl && <Box  bgImage={`url(${tmdbConfig.companyImgUrl(imgUrl)})`} h='100px' w='150px' bgPos={'center'} bgSize='contain' bgRepeat={'no-repeat'} objectFit={'cover'}></Box>}
           <Text>{name }</Text>
     </Flex>
   )
