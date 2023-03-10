003b82e8ee3e0774c1e8f7d7e66087ef3ff403f4
Mufadal03 <sadrimufadal@gmail.com>
2023-03-10 22:20:24 +0530

    background

--- client/src/components/GenreTypeCard.jsx
+++ client/src/components/GenreTypeCard.jsx
@@ -1,9 +1,9 @@
 import { Text } from '@chakra-ui/react'
 import React from 'react'
 
-const GenreTypeCard = ({name}) => {
+const GenreTypeCard = ({ name }) => {
   return (
-      <Text p={'2px 6px '} border={'1px solid rgba(115, 114, 114, 0.5)'} style={{ backgroundImage: `linear-gradient(190deg, rgba(34,193,195,0) 0%, rgba(0,0,0,0.9024859943977591) 100%)` }}  fontSize='md'>{name}</Text>
+    <Text p={'2px 6px '} bgColor="rgba(115, 114, 114, 0.5)" border={'1px solid rgba(115, 114, 114, 0.5)'}   fontSize='md'>{name}</Text>
   )
 }
 
