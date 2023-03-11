f04f377d78cc1b1ddfd0116c6fa297cb240b7d7a
Mufadal03 <sadrimufadal@gmail.com>
2023-03-11 19:35:09 +0530

    Fetching location changed

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,8 +1,37 @@
 import { Box } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import mediaApi from '../api/modules/media.api'
 import tmdbConfig from '../api/tmdb/tmdb.config'
+import HeroLoading from './Loaders/HeroLoading'
 
-const Hero = ({ data }) => {
+const Hero = ({ api }) => {
+  const [data, setData] = useState()
+  const [loading, setLoading] = useState(false)
+  const {mediaType} = useParams()
+  useEffect(() => {
+    const FetchMedia = async () => {
+      try {
+        setLoading(true)
+        const response = await api
+        const details = await mediaApi.getDetail({ mediaType: mediaType || tmdbConfig.mediaType.movie, mediaId: response.results[0].id })
+        setLoading(false)
+        setData({
+          backdrop: details.backdrop_path,
+          genres: details.genres,
+          description: details.overview,
+          runtime: details.runtime,
+          title: details.title,
+          rating: details.vote_average,
+          video: details.video.results?.find((el) => el.type ==='Trailer')
+        })
+      } catch (error) {
+        console.log('show error on toast')
+      }
+    }
+    FetchMedia()
+  },[mediaType])
+  if(loading)return <HeroLoading />
     return (
       <Box h='100vh'>
         {
