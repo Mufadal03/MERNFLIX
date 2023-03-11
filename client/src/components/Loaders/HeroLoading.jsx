7d0e97c9bc271a63da8eaedf3c7aa6519e418857
Mufadal03 <sadrimufadal@gmail.com>
2023-03-11 19:38:16 +0530

    basic loading skeleton comp

---
+++ client/src/components/Loaders/HeroLoading.jsx
@@ -0,0 +1,10 @@
+import { Box, Flex } from '@chakra-ui/react'
+import React from 'react'
+
+const HeroLoading = () => {
+  return (
+    <Flex justifyContent={'center'} alignItems='center' minH='100vh'>HeroLoading</Flex>
+  )
+}
+
+export default HeroLoading
---
+++ client/src/components/Loaders/RowLoading.jsx
@@ -0,0 +1,10 @@
+import { Flex } from '@chakra-ui/react'
+import React from 'react'
+
+const RowLoading = () => {
+  return (
+      <Flex h='300px' border={'1px solid white'} justifyContent='center' alignItems={'center'}>RowLoading</Flex>
+  )
+}
+
+export default RowLoading
