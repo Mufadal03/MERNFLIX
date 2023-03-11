7579510838ef3f5b9907e19e192648090eaa10e9
Mufadal03 <sadrimufadal@gmail.com>
2023-03-11 19:40:30 +0530

    Person detail page

--- client/src/pages/PersonDetail.jsx
+++ client/src/pages/PersonDetail.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { Box, Flex } from '@chakra-ui/react'
+import React  from 'react'
+import PersonAbout from '../components/PersonAbout'
+import PersonMedia from '../components/PersonMedia'
 
 const PersonDetail = () => {
   return (
-    <div>PersonDetail</div>
+    <Box bgColor={'black'} minH='100vh' color={'white'} >
+      <Flex direction={'column'} w='90vw' m='auto'>
+        <PersonAbout />
+        <PersonMedia />
+      </Flex>      
+    </Box>
   )
 }
 
