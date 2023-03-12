22cc14deb76aecc69b8b95b6a3f23315adf96185
Mufadal03 <sadrimufadal@gmail.com>
2023-03-12 22:27:56 +0530

    category changed

--- client/src/components/Genre.jsx
+++ client/src/components/Genre.jsx
@@ -10,10 +10,10 @@ const Genre = () => {
           <GenreRow title={'trending Tv shows'} api={mediaApi.getTrendingList({mediaType:'tv',timeWindow:'day'}) } to='/media/tv?category=trending' />
           <GenreRow title={'popular movies' } api={mediaApi.getList({mediaType:'movie',mediaCategory:'popular',page:1})} to='/media/movie?category=popular' />
           <GenreRow title={'popular tv shows'} api={ mediaApi.getList({mediaType:'tv',mediaCategory:'popular',page:1})} to='/media/tv?category=popular'/>
-          <GenreRow title={'Science Fiction movies'} api={mediaApi.getMediaByGenre({ mediaType: 'movie', genreId: '878' })} to='/media/movie?category=sci-fic' />
-          <GenreRow title={'thriller movies'} api={mediaApi.getMediaByGenre({ mediaType: 'movie', genreId: '53' })} to='/media/movie?category=thriller'/> 
-          <GenreRow title={'Animation movies' } api={mediaApi.getMediaByGenre({mediaType:'movie',genreId:'16'})} to='/media/movie?category=animation'/> 
-          <GenreRow title={'Documentary' } api={mediaApi.getMediaByGenre({mediaType:'tv',genreId:'99'})} to='/media/tv?category=documentory'/>
+          <GenreRow title={'Science Fiction movies'} api={mediaApi.getMediaByGenre({ mediaType: 'movie', genreId: '878' })} to='/media/movie?category=Science Fiction' />
+          <GenreRow title={'thriller movies'} api={mediaApi.getMediaByGenre({ mediaType: 'movie', genreId: '53' })} to='/media/movie?category=Thriller'/> 
+          <GenreRow title={'Animation movies' } api={mediaApi.getMediaByGenre({mediaType:'movie',genreId:'16'})} to='/media/movie?category=Animation'/> 
+          <GenreRow title={'Documentary' } api={mediaApi.getMediaByGenre({mediaType:'tv',genreId:'99'})} to='/media/tv?category=Documentary'/>
           
     </Flex>
   )
