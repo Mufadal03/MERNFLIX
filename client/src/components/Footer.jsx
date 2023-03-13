02f33219dd95db3a44a4e907b943213d2870fcba
Mufadal03 <sadrimufadal@gmail.com>
2023-03-13 19:00:45 +0530

    initial footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import { Box, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const Footer = () => {
+  return (
+      <Box border='1px solid red'  bgColor='black' color={'white'}>
+         <Text textAlign={'center'}>Made by Mufadal add code and portfolio</Text>
+    </Box>
+  )
+}
+
+export default Footer
