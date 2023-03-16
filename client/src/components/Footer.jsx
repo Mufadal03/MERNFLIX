0d40bf1b85cb2ac62a15536accbfe85609fe22da
Mufadal03 <sadrimufadal@gmail.com>
2023-03-16 15:03:18 +0530

    footer details added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,12 @@
-import { Box, Text } from '@chakra-ui/react'
+import { HStack, Text, Icon, Link } from '@chakra-ui/react'
+import {AiFillHeart,AiFillGithub} from 'react-icons/ai'
 import React from 'react'
 
 const Footer = () => {
   return (
-      <Box border='1px solid red'  bgColor='black' color={'white'}>
-         <Text textAlign={'center'}>Made by Mufadal add code and portfolio</Text>
-    </Box>
+    <HStack bgColor={'black'} justifyContent='center' color='white' py='1rem'>
+      <Text>Made with </Text><Icon color='red' as={AiFillHeart} /><Text>by <Link href='http://mufadal03.github.io' isExternal>Mufadal Sadri</Link></Text><Link href='https://github.com/Mufadal03' isExternal ><Icon h='5' w='5' as={AiFillGithub}/></Link>
+    </HStack>
   )
 }
 
