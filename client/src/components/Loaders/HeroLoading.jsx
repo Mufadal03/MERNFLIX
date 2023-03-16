10c429a92f97e49212f22c012bb7a28a76daec65
Mufadal03 <sadrimufadal@gmail.com>
2023-03-16 15:02:45 +0530

    Loading components

--- client/src/components/Loaders/HeroLoading.jsx
+++ client/src/components/Loaders/HeroLoading.jsx
@@ -1,9 +1,11 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Flex, Image } from '@chakra-ui/react'
 import React from 'react'
-
+import 'animate.css';
 const HeroLoading = () => {
   return (
-    <Flex justifyContent={'center'} alignItems='center' minH='100vh'>HeroLoading</Flex>
+    <Flex justifyContent={'center'} alignItems='center' minH='100vh' bgColor={'black'}>
+      <Image className='animate__animated animate__slideInLeft' src='https://fontmeme.com/permalink/230305/45fcbc47916afa4eed29a10ff819946b.png' alt='Hero Loading'/>
+    </Flex>
   )
 }
 
--- client/src/components/Loaders/RowLoading.jsx
+++ client/src/components/Loaders/RowLoading.jsx
@@ -1,9 +1,53 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Flex, Image } from '@chakra-ui/react'
 import React from 'react'
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Scrollbar, A11y } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
 const RowLoading = () => {
   return (
-      <Flex h='300px' border={'1px solid white'} justifyContent='center' alignItems={'center'}>RowLoading</Flex>
+    <Flex h='100%' justifyContent='center' alignItems={'center'}>
+      <Swiper
+              modules={[Scrollbar, A11y]}
+              spaceBetween={10}
+              slidesPerView={8}
+              breakpoints={{
+                  100: {
+                       slidesPerView: 3,
+                        spaceBetween: 10,
+                  },
+                    640: {
+                        slidesPerView: 4,
+                        spaceBetween: 10,
+                    },
+                    768: {
+                        slidesPerView: 5,
+                        spaceBetween: 10,
+                    },
+                    1024: {
+                        slidesPerView: 6,
+                        spaceBetween: 10,
+                  },
+                  1025: {
+                      slidesPerView: 8, 
+                      spaceBetween:10
+                    }
+        }}
+          >
+              {
+                  new Array(10).fill(0).map((el, i) => {
+                      return (
+                        <SwiperSlide key={i}>
+                          <Flex alignItems={'center'} h='268px' border="1px solid rgba(115, 114, 114, 0.5)">
+                            <Image className='animate__animated animate__pulse' src='https://fontmeme.com/permalink/230305/45fcbc47916afa4eed29a10ff819946b.png' alt='loadingIMG'/>
+                          </Flex>
+                          </SwiperSlide>
+                      )
+                  })
+              }
+          </Swiper>
+      </Flex>
   )
 }
 
