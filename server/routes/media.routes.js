46288b5bfd5e471555e828a6f599df20b975ad3c
Mufadal03 <sadrimufadal@gmail.com>
2023-03-20 22:54:28 +0530

    search route and user route added

--- server/routes/media.routes.js
+++ server/routes/media.routes.js
@@ -1,17 +1,16 @@
 const { Router } = require('express')
 const { getList, getDetail, getSearch, getGenre, getTrendingList, getMediaByGenre} = require('../controller/media.contorller')
 mediaRoutes = Router()
+mediaRoutes.get('/search/:mediaType', getSearch)
+
 mediaRoutes.get('/discover/:mediaType',getMediaByGenre)
 
 mediaRoutes.get('/:mediaType/:mediaCategory',getList)
 
 mediaRoutes.get('/detail/:mediaType/:mediaId', getDetail)
 
-mediaRoutes.get('/find', getSearch)
-
 mediaRoutes.get('/genre', getGenre)
 
 mediaRoutes.get('/trending/:mediaType/:timeWindow', getTrendingList)
-
 
 module.exports={mediaRoutes}
