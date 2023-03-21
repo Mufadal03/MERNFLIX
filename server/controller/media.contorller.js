047445ac35fbd3fbd963145107e03c50d1c30a27
Mufadal03 <sadrimufadal@gmail.com>
2023-03-21 21:28:07 +0530

    storing token in cookies

--- server/controller/media.contorller.js
+++ server/controller/media.contorller.js
@@ -18,7 +18,7 @@ const getList = async(req, res) => {
 const getDetail = async (req, res) => {
     const { mediaType, mediaId } = req.params
     const payload = { mediaType, mediaId }
-    
+   
    try {
        const media = await tmdbApi.mediaDetail(payload)
        media.video = await tmdbApi.mediaVideos(payload)
@@ -28,23 +28,13 @@ const getDetail = async (req, res) => {
        const similar = await tmdbApi.similarMedia(payload)
        media.similar = similar.results
        media.images = await tmdbApi.mediaImages(payload)
-       media.watchProvide = await tmdbApi.mediaWatchProvider(payload)
+       media.watchProvider = await tmdbApi.mediaWatchProvider(payload)
+      
        res.status(200).send(media)
    } catch (error) {
        console.log(error)
     res.status(400).send({success:false,message:error})
    }
-}
-
-const getSearch = async(req, res) => {
-    const { type, query, page = 1 } = req.query 
-    const payload = { mediaType: type, query, page }
-    try {
-        const data = await tmdbApi.mediaSearch(payload)
-        res.status(200).send(data)
-    } catch (error) {
-        res.status(400)
-    }
 }
 
 const getGenre = async (req, res) => {
@@ -60,7 +50,8 @@ const getGenre = async (req, res) => {
 
 const getTrendingList = async(req, res) => {
     const { mediaType, timeWindow } = req.params 
-    const payload = { mediaType, timeWindow }
+    const {page=1} = req.query
+    const payload = { mediaType, timeWindow ,page}
     try {
         const data = await tmdbApi.trendingList(payload)
         res.status(200).send(data)
@@ -71,10 +62,20 @@ const getTrendingList = async(req, res) => {
 
 const getMediaByGenre = async(req, res) => {
     const { mediaType } = req.params 
-    const { with_genres } = req.query 
-    console.log(req.params)
+    const { with_genres ,page} = req.query 
     try {
-        const data = await tmdbApi.medaiByGenre({ mediaType, with_genres })
+        const data = await tmdbApi.medaiByGenre({ mediaType, with_genres,page })
+        res.status(200).send(data)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+const getSearch = async (req, res) => {
+    const { mediaType } = req.params 
+    const {query,page} = req.query 
+    try {
+        const data = await tmdbApi.mediaSearch({ mediaType, query, page })
         res.status(200).send(data)
     } catch (error) {
         res.status(400).send(error)
--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -2,41 +2,63 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const { userModel } = require('../model/user.model')
 
-const signUp = (req, res) => {
+const signUp = async(req, res) => {
     const { email, profilePic = '', password, username } = req.body
+    const user = await userModel.findOne({ email })
+    if(user!==null)return res.status(400).send({response:"User already exist",success:false})
     try {
         bcrypt.hash(password, 5, async(err, hash) => {
             if (err) {
-               return res.status(400).send(err)
+               return res.status(400).send({response:err.message,success:false})
             }
             const user = new userModel({ email, profilePic, password: hash, username })
             await user.save()
-            res.status(201).send({response:"User created"})
+            res.status(201).send({response:"User created",success:true})
        })
     } catch (error) {
-        res.status(500).send({response:'Internal server Error'})
+        res.status(500).send({response:error.message,success:false})
     }
 }
 
 const signIn = async(req, res) => {
     const { email, password } = req.body
     const user = await userModel.findOne({ email })
-    if(!user)return res.status(401).send({response:'Please Signup'})
+    if(!user)return res.status(401).send({response:'Please Signup',success:false})
     const hashedPassword = user.password
     try {
         bcrypt.compare(password, hashedPassword, (err, result) => {
-            if (err) return res.status(400).send({ response: 'Credentials are not matching' })
+            if (err) return res.status(400).send({ response: 'Credentials are not matching' ,success:false})
             if (result) {
                 jwt.sign({ userId: user._id, userEmail: user.email }, process.env.JWT_SECRET, (err, token) => {
-                    console.log(token) 
-                    res.status(200).send({response:'SignIn Successfull',token})
+                    res.cookie('jwttoken', token, {
+                        httpOnly: false,
+                        expires: new Date(Date.now() + 2589200000),
+                        origin: 'http://localhost:3000',
+                        sameSite: 'none',
+                        secure: true
+                    })
+                    res.status(200).send({response:'SignIn Successfull',token,success:true})
                 })
             }
         })
     } catch (error) {
-        res.status(400).send({response:'OOPS Something went wrong'})
+        res.status(400).send({response:'OOPS Something went wrong',success:false})
+    }
+}
+
+const userInfo = async(req,res) => {
+    const { userId } = req.body 
+    try {
+        const user = await userModel.findById({ '_id': userId })
+        console.log(user)
+        const response = {}
+        response.username=user.username
+        res.status(200).send(response)
+    } catch (error) {
+        console.log(error)
+        res.status(400).send({response:error.message,success:false})
     }
 }
 
 
-module.exports={signUp,signIn}
+module.exports={signUp,signIn,userInfo}
