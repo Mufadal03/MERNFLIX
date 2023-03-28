0d04fa99fbbd11f273d7a7ce679180c657865ad9
Mufadal03 <sadrimufadal@gmail.com>
2023-03-28 23:02:01 +0530

    favourites route added

--- client/src/routes/AllRoutes.jsx
+++ client/src/routes/AllRoutes.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
 import {Route, Routes} from 'react-router-dom'
 import Home from '../pages/Home'
 import MediaDetail from '../pages/MediaDetail'
 import PersonDetail from '../pages/PersonDetail'
 import MediaByGenre from '../pages/MediaByGenre'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+import HOC from '../components/HOC'
+import MediaSearch from '../pages/MediaSearch'
+import Favourite from '../pages/Favourite'
+import PrivateRoute from '../components/PrivateRoute'
 const AllRoutes = () => {
   return (
       <>
-          <Navbar />
           <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/media/:mediaType' element={<MediaByGenre/>} /> 
-              <Route path='/detail/:mediaType/:mediaId' element={<MediaDetail />} /> 
-              <Route path='/person/:personId' element={<PersonDetail />} />
+              <Route path='/' element={<HOC><Home /></HOC>} />
+              <Route path='/media/:mediaType' element={<HOC><MediaByGenre/></HOC>} /> 
+              <Route path='/detail/:mediaType/:mediaId' element={<HOC><MediaDetail /></HOC>} /> 
+              <Route path='/person/:personId' element={<HOC><PersonDetail /></HOC>} />
+              <Route path='/search' element={<HOC><MediaSearch /></HOC>} />
+              <Route path='/favourites' element={<PrivateRoute><HOC><Favourite /></HOC></PrivateRoute>}/>
+              <Route path='/account/login' element={<Login />} />
+              <Route path='/account/signup' element={<Signup/>} />
               <Route path='*' element={<h1>404 notfound</h1>} />
-            </Routes>
+      </Routes>
       </>
   )
 }
