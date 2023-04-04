898107a607b48ab62244a1a18ad2d95216ad431b
Mufadal03 <sadrimufadal@gmail.com>
2023-04-04 22:41:10 +0530

    time log added to reviews

--- client/src/components/MediaDetail/Comment.jsx
+++ client/src/components/MediaDetail/Comment.jsx
@@ -1,24 +1,16 @@
 import { Avatar, Flex, Text } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react'
-
+import React from 'react'
+import dayjs from 'dayjs'
 const Comment = ({ data }) => {
- const [date,setDate] = useState('')
-  useEffect(() => {
-    const d = new Date(data.createdAt)
-    // console.log(Date.now(d))
-    // console.log(Date.now())
-    // console.log(d)
-    // console.log(new Date(Date.now()))
-    setDate(d)
-  },[data])
   return (
    <Flex gap='.5rem' py='1rem'>
           <Avatar size={'sm'} name={ data.author.username} />
           <Flex direction={'column'} gap='.5rem'>
-        <Text fontSize={'sm'}>{data.author.username}
-          <Text as='span' fontSize={'xs'} fontWeight='thin' color='gray'>{data.createdAt}</Text>
-          </Text>
-             <Text fontSize={'sm'}>{data.comment}</Text>
+        <Flex alignItems={'center'} gap='.5rem'> 
+          <Text fontSize={'sm'}>{data.author.username}</Text>
+          <Text as='span' fontSize={'xs'} fontWeight='thin' color='gray'>{dayjs(data.createdAt).format("DD-MM-YYYY HH:mm:ss")}</Text>
+            </Flex>
+          <Text fontSize={'sm'}>{data.comment}</Text>
           </Flex>
    </Flex>
   )
--- client/src/components/MediaDetail/Reviews.jsx
+++ client/src/components/MediaDetail/Reviews.jsx
@@ -4,35 +4,54 @@ import Comment from './Comment'
 import LoadMore from '../Common/LoadMore'
 import {BiSend} from 'react-icons/bi'
 import { useSelector } from 'react-redux'
-
-const Reviews = ({mediaName,allReviews}) => {
+import {useParams} from 'react-router-dom'
+import { reviewApi } from '../../api/modules/review.api'
+const Reviews = ({mediaName}) => {
     const { isAuth, username } = useSelector(state => {
         return {
             isAuth: state.isAuth,
             username:state.username
         }
     })
+    const {mediaType,mediaId} = useParams()
     const [userReview,setUserReview] = useState('')
-    const [reviewsDb,setReviewDb] = useState(allReviews)
+    const [reviewsDb,setReviewDb] = useState([])
     const [reviews, setReviews] = useState([])
     const [page,setPage] = useState(1)
     const skip = 3
+
+    const FetchReviews = async () => {
+        try {
+            const {response} = await reviewApi.getReviews({ mediaType, mediaId })
+            setReviewDb([...response])    
+            const filtered = response.slice(0, skip)  
+            setReviews(filtered)
+        } catch (error) {
+            console.log(error)
+        }
+    }
     useEffect(() => {
-        const filtered = reviewsDb.slice(0, skip)  
-        setReviews(filtered)
-    }, [])
+        FetchReviews()
+    }, [mediaId, mediaType])
+    
     const loadMore = () => {
         console.log('load more comments')
         setReviews([...reviews, ...[...reviewsDb].splice(page * skip, skip)])
         setPage(p=>p+1)
     }
-    const handlePost = () => {
+    const handlePost = async() => {
         if (userReview === '') return
-        console.log(userReview)
+        try {
+            const { response } = await reviewApi.postReview({ mediaType, mediaId, comment: userReview })
+            setUserReview('')
+            FetchReviews()
+        } catch (error) {
+            console.log(error)
+        }
     }
   return (
-      <Box w='90vw' m='auto'>
-          <Heading fontWeight={'md'} fontSize='3xl' fontFamily={'bebas'} py='1rem'>Reviews</Heading>
+      <Box w='90vw' m='auto' id='review'>
+          <Heading fontWeight={'md'} fontSize='3xl' fontFamily={'bebas'} py='1rem'>Reviews ({reviewsDb.length})</Heading>
           <Flex direction={'column'} gap='.5rem'>
               {
                   reviews.map((el) => <Comment key={el.id} data={ el} />)
@@ -47,7 +66,7 @@ const Reviews = ({mediaName,allReviews}) => {
                   <Avatar name={username}/>
                   <Text>{username}</Text>
               </Flex>
-              <Textarea value={userReview} onChange={(e)=>setUserReview(e.target.value)} size={'sm'} placeholder={`write a review for ${mediaName}`} />
+              <Textarea   value={userReview} onChange={(e)=>setUserReview(e.target.value)} size={'sm'} placeholder={`write a review for ${mediaName}`} />
                   
 
               <Button onClick={handlePost} rightIcon={<Icon as={BiSend} h={5} w={6} />} border='1px solid rgba(255, 0, 0, 0.5)' _hover={{backgroundColor:'transparent'}} bgColor='transparent' borderRadius={'none'} w='fit-content' fontWeight={'thin'} color='red'>POST</Button>
